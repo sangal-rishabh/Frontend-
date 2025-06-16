1c4070c9542db1986ae1373be6b281634cd07529
sangal-rishabh <sangalrishabh19@gmail.com>
2025-06-16 09:03:55 +0530

    Update login.jsx

--- src/Components/Login/login.jsx
+++ src/Components/Login/login.jsx
@@ -20,7 +20,7 @@ const Login = ({ setLoginModel }) => {
     console.log("Sending login data:", loginField); // Debug log
     try {
       const response = await axios.post(
-        "https://backend-3-858w.onrender.com/auth/login",
+        "https://backend-service-1j0i.onrender.com/auth/login",
         loginField,
         { withCredentials: true }
       );
